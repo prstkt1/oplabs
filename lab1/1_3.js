df1df1ef128d3cbaeb7af4b37ae79e940373a6d7
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2024-12-02 22:04:09 +0200

    Update 1_3.js

--- lab1/1_3.js
+++ lab1/1_3.js
@@ -1,12 +1,15 @@
-const arr = [true, 'hello', 11, 55, -300, false, false, 'word', null, undefined, 42, 'example', true,'word'];
-const typeCount = {};
+const arr = [true, 'hello', 11, 55, -300, false, false, 'word', null, undefined, 42, 'example', true, 'word'];
 
-for (const item of arr) {
-    const type = typeof item;
-    if (!typeCount[type]) {
-        typeCount[type] = 0;
-    }
-    typeCount[type]++;
+const typeCount = {
+    number: 0,
+    string: 0,
+    boolean: 0,
+    object: 0,      
+    undefined: 0    
+}
+for (const element of arr) {
+    const type = typeof element;
+    typeCount[type] += 1;
 }
 
 console.dir(typeCount);
