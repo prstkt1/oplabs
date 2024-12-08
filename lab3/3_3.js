026b34ea4892bdabf0c329f17a501bc1c8e8a618
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2024-12-09 01:32:54 +0200

    Update 3_3.js

--- lab3/3_3.js
+++ lab3/3_3.js
@@ -1,9 +1,11 @@
-function ipToNumber(ip = '127.0.0.1') {
+const ipToNumber = (ip = '127.0.0.1') => {
     const parts = ip.split('.');
     let result = 0;
     for (let i = 0; i < parts.length; i++) {
         const byte = parseInt(parts[i]);
         result = (result << 8) + byte;
+    }
     return result;
 }
-console.log(ipToNumber())
+
+console.log(ipToNumber()); 
