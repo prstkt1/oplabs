732ba55629eb7b61e8cc247ccf7ea5624566d800
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2024-12-11 17:54:45 +0200

    Update 4_1.js

--- lab4/4_1.js
+++ lab4/4_1.js
@@ -1,6 +1,13 @@
 const sum = (...args) => {
-    let result = 0;
-    for(){
+  let result = 0;
+  for (let i = 0; i < args.length; i++) {
+    result += args[i];
+  }
+  return result;
+};
 
-    };
-}
+console.log(sum(1, 2, 3));
+console.log(sum(0));
+console.log(sum());
+console.log(sum(1, -1, 1));
+console.log(sum(10, -1, -1, -1));
