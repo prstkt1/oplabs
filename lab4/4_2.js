e7ab4b4d3cba54c73a80aadfd87fb7c7e83e52e1
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2024-12-11 18:22:19 +0200

    Create 4_2.js

---
+++ lab4/4_2.js
@@ -0,0 +1,13 @@
+const sum = (...args) => {
+  let result = 0;
+  for (let i of args) {
+    result += i;
+  }
+  return result;
+};
+
+console.log(sum(1, 2, 3));
+console.log(sum(0));
+console.log(sum());
+console.log(sum(1, -1, 1));
+console.log(sum(10, -1, -1, -1));
